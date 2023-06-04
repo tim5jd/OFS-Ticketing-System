86ca04d2f9edf0ef8daba37a474943d8fcfcd5c0
Ryantdebolt <62964601+Ryantdebolt@users.noreply.github.com>
2023-06-04 02:10:05 -0400

    Now with PDF's!

--- OFS Ticket System.py
+++ OFS Ticket System.py
@@ -1,10 +1,16 @@
+"""Scale ticket system for Ohio Foundation Seeds."""
+
 # Imports
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter
 import sys
 from PyQt5 import QtWidgets
 import datetime
 import csv
 
+
 def window():
+    """Open a window to take in user inputs."""
     app = QtWidgets.QApplication(sys.argv)
     win = QtWidgets.QMainWindow()
     win.setGeometry(1200, 300, 720, 720)
@@ -12,10 +18,11 @@ def window():
     win.show()
     sys.exit(app.exec_())
 
-window()
+
+# window()
 
 # Collect Scale Ticket Number
-tickets =  []
+tickets = []
 suppliers = []
 with open("tickets.csv") as t:
     csv_reader = csv.reader(t, delimiter=',')
@@ -23,156 +30,215 @@ with open("tickets.csv") as t:
         tickets.append(int(row[0]))
         suppliers.append(str(row[1]))
 t.close()
-#print(suppliers)
-Scale_Ticket = max(tickets)+1
+
+# rint(suppliers)
+scale_ticket = max(tickets)+1
 
 # Read in data and verify its correctness
 
-Verified = ("no") # while this is no it will reprompt 
+verified = ("no")
+# while verified is no, it will reprompt
 
-while(Verified == "no"):
+while verified == "no":
     # Ask and Read in data
 
-    #Read in the current date and time
+    # Read in the current date and time
     date = datetime.datetime.now()
-    Date = str(str(date.day) +"/"+ str(date.month) + "/" + str(date.year))
-    txtdate = str(str(date.day) +"_"+ str(date.month) + "_" + str(date.year))
-    Time = str(str(date.hour) + ":" + str(date.minute))
-    print(Date)
-    
+    date_str = f"{date.day}/{date.month}/{date.year}"
+    txtdate = f"{date.day}_{date.month}_{date.year}"
+    time = f"{date.hour}:{date.minute}"
+    print(date_str)
+
     # Read in seed
-    Seed = str(input("Please Enter the Seed Type: "))
-    Seed = Seed.lower()
-    
+    seed = str(input("Please Enter the Seed Type: "))
+    seed = seed.lower()
+
     # Check to make sure seed is valid type
-    Seed_check = False
-    while(Seed_check == False):
-        if (Seed != "barley" and Seed != "wheat" and Seed != "oats" and Seed != "soybeans" and Seed != "soybean"):
+    seed_check = False
+    while seed_check is False:
+        if (seed != "barley" and seed != "wheat" and seed != "oats"
+           and seed != "soybeans" and seed != "soybean"):
             print("Invalid Seed type provided.")
-            Seed = str(input("Please Enter the Seed Type: "))
-            Seed = Seed.lower()
+            seed = str(input("Please Enter the Seed Type: "))
+            seed = seed.lower()
         else:
-            Seed_check = True
-    
-    #Read in the variety
-    Variety = input("Please enter variety code: ")
+            seed_check = True
 
-    #Read in the supplier
-    Supplier = input("Please enter supplier: ")
+    # Read in the variety
+    variety = input("Please enter variety code: ")
 
-    #Read in load number
-    Load_Num = input("Please enter load number: ")
+    # Read in the supplier
+    supplier = input("Please enter supplier: ")
 
-    #Read in weights
+    # Read in load number
+    load_num = input("Please enter load number: ")
+
+    # Read in weights
     weight_check = False
-    while(weight_check == False):
-        Gross_W = float(input("Please enter gross weight in lbs: "))
+    while weight_check is False:
+        gross_weight = float(input("Please enter gross weight in lbs: "))
 
-        Tare_W = float(input("Please enter Tare weight in lbs: "))
-        
-        if (Gross_W <= Tare_W):
+        tare_weight = float(input("Please enter Tare weight in lbs: "))
+
+        if gross_weight <= tare_weight:
             print("Gross weight cannot be less than tare weight.")
         else:
             weight_check = True
 
-    Net_W = Gross_W - Tare_W
+    net_weight = gross_weight - tare_weight
 
     # Read in wagon ID
-    Wagon = input("Please enter the Truck/Wagon ID: ")
+    wagon = input("Please enter the Truck/Wagon ID: ")
 
     # Read in destination, and check to make sure it is viable
-    Destination = input("Please enter destination: ")
+    destination = input("Please enter destination: ")
 
     # Read in moisture level
-    Moisture = float(input("Please enter Moisture percentage: "))
+    moisture = float(input("Please enter Moisture percentage: "))
 
     # read in FM level
-    FM = float(input("Please enter FM percentage: "))
-    
+    foreign_matter = float(input("Please enter FM percentage: "))
+
     # read in cleanout
-    Cleanout = float(input("Please enter the cleanout percentage: "))
-    
+    cleanout = float(input("Please enter the cleanout percentage: "))
+
     # Read in test weight
-    TW = float(input("Please enter Test Weight: "))
-    
-    #Calculate the Bushels
-    if (Seed == "wheat" or Seed == "soybeans"):
-        Gross_B = Net_W/60
-    elif(Seed == "barley"):
-        Gross_B = Net_W/48
-    elif(Seed == "oats"):
-        Gross_B = Net_W/32
-        
-    Gross_B = round(Gross_B, 2)
-    
-    #  if FM is greater than 1% then:  Gross Bushels - (FM-1%)*Gross Bushels = Net Bushels
-    if (FM > 1.0):
-        Abs_FM = FM/100.0
-        Net_B = Gross_B - Gross_B*(Abs_FM-0.01)
+    test_weight = float(input("Please enter Test Weight: "))
+
+    # Calculate the Bushels
+    if seed == "wheat" or seed == "soybeans":
+        gross_bushels = net_weight / 60.0
+    elif seed == "barley":
+        gross_bushels = net_weight / 48.0
+    elif seed == "oats":
+        gross_bushels = net_weight / 32.0
+
+    gross_bushels = round(gross_bushels, 2)
+
+    # if FM is greater than 1%
+    # then:  Gross Bushels - (FM-1%)*Gross Bushels = Net Bushels
+    if foreign_matter > 1.0:
+        foreign_matter_percent = foreign_matter / 100.0
+        net_bushels = (gross_bushels
+                       - gross_bushels * (foreign_matter_percent - 0.01))
     else:
-        Net_B = Gross_B
-        
-    #  Net Bushels = Net Bushels(100% - cleanout)
-    Abs_Cleanout = Cleanout/100.0
-    Net_B = round(Net_B*(1.0-Abs_Cleanout), 2)
-    
-    
-    #print data for verification
-    
+        net_bushels = gross_bushels
+
+    # Net Bushels = Net Bushels(100% - cleanout)
+    cleanout_percent = cleanout/100.0
+    net_bushels = round(net_bushels * (1.0 - cleanout_percent), 2)
+
+    # print data for verification
     print()
-    print("Please read in the following information and verify it is correct: ")
+    print("Please read in the following information and verify it is correct:")
     print()
-    print("Load #:        ", Load_Num)
-    print("Load of:       ", Seed, Variety)
-    print("From:          ", Supplier)
-    print("Wagon/Truck ID:", Wagon)
-    print("To:            ", Destination)
-    print("% Moisture:    ", Moisture)
-    print("% FM:          ", FM)
-    print("% Cleanout:    ", Cleanout)
-    print("Test Weight:   ", TW)
-    print("Gross Weight:  ", Gross_W, "lbs")
-    print("Tare Weight:   ", Tare_W, "lbs")
-    print("Net Weight:    ", Net_W, "lbs")
-    print("Gross Units:   ", Gross_B, "BU")
-    print("Net Units      ", Net_B, "BU")
+    print("Load #:        ", load_num)
+    print("Load of:       ", seed, variety)
+    print("From:          ", supplier)
+    print("Wagon/Truck ID:", wagon)
+    print("To:            ", destination)
+    print("% Moisture:    ", moisture)
+    print("% FM:          ", foreign_matter)
+    print("% Cleanout:    ", cleanout)
+    print("Test Weight:   ", test_weight)
+    print("Gross Weight:  ", gross_weight, "lbs")
+    print("Tare Weight:   ", tare_weight, "lbs")
+    print("Net Weight:    ", net_weight, "lbs")
+    print("Gross Units:   ", gross_bushels, "BU")
+    print("Net Units      ", net_bushels, "BU")
     print()
 
-    Verified = input("If this information is correct, please enter your name: \nIf this information is not correct, please enter no: ")
+    print("If this information is correct, please enter your name:")
+    print("If this information is not correct, please enter no:")
+    verified = input()
 
 # Print information to file
 # Try to make this a PDF
 
-with open(str(Scale_Ticket) + "_" + str(Supplier) + "_" + str(txtdate) + '.txt', 'w') as ticket:
-    ticket.write("Ohio Foundation Seeds, Inc. \n")
-    ticket.write("11491 Foundation Road, PO Box 6 \n")
-    ticket.write("Croton, Ohio 43013 \n")
-    ticket.write("Phone: 740-893-2501 \n")
-    ticket.write("\n")
-    ticket.write("Ticket Number:  "+ str(Scale_Ticket)+ "\n")
-    ticket.write("Date:           "+ str(Date) + " " + str(Time) + "\n")
-    ticket.write("\n")
-    ticket.write("Load #:         "+ str(Load_Num)+ "\n")
-    ticket.write("Load of:        "+ str(Seed) + " " +str(Variety)+ "\n")
-    ticket.write("From:           "+ str(Supplier)+ "\n")
-    ticket.write("Wagon/Truck ID: "+ str(Wagon)+ "\n")
-    ticket.write("To:             "+ str(Destination)+ "\n")
-    ticket.write("\n")
-    ticket.write("% Moisture:     "+ str(Moisture)+ "\n")
-    ticket.write("% FM:           "+ str(FM)+ "\n")
-    ticket.write("% Cleanout:     "+ str(Cleanout)+ "\n")
-    ticket.write("Test Weight:    "+ str(TW)+ "\n")
-    ticket.write("Gross Weight:   "+ str(Gross_W) + " lbs"+ "\n")
-    ticket.write("Tare Weight:    "+ str(Tare_W) + " lbs"+ "\n")
-    ticket.write("Net Weight:     "+ str(Net_W) + " lbs"+ "\n")
-    ticket.write("\n")
-    ticket.write("Gross Units:    "+ str(Gross_B) + " BU"+ "\n")
-    ticket.write("Net Units:      "+ str(Net_B) + " BU"+ "\n")
-    ticket.write("\n")
-    ticket.write("Verified By:    "+ str(Verified)+ "\n")             
-ticket.close()
+ticket = canvas.Canvas(f"{scale_ticket}_{supplier}_{txtdate}.pdf",
+                       pagesize=letter)
+# Draw boxes
+ticket.setLineWidth(.5)
+ticket.setStrokeColorRGB(0.2, 0.5, 0.3)
+ticket.rect(15, 685, 580, 90, stroke=1, fill=0)
+ticket.rect(15, 15, 580, 670, stroke=1, fill=0)
+
+# Write header
+ticket.setLineWidth(.3)
+ticket.setStrokeColorRGB(0.0, 0.0, 0.0)
+ticket.setFont('Times-Roman', 18)
+ticket.drawString(30, 750, "Ohio Foundation Seeds, Inc.")
+ticket.setFont('Times-Roman', 12)
+ticket.drawString(30, 730, "11491 Foundation Road, PO Box 6")
+ticket.drawString(30, 715, "Croton, Ohio 43013")
+ticket.drawString(30, 700, "Phone: 740-893-2501")
+ticket.drawString(475, 730, "Date:")
+ticket.drawString(510, 730, f"{date_str}")
+ticket.line(505, 727, 580, 727)
+ticket.drawString(475, 715, "Time:")
+ticket.drawString(510, 715, f"{time}")
+ticket.line(505, 712, 580, 712)
+ticket.drawString(475, 700, "Ticket Number:")
+ticket.drawString(560, 700, f"{scale_ticket}")
+ticket.line(555, 697, 580, 697)
+
+# Write load information
+ticket.drawString(30, 655, "Load #:")
+ticket.drawString(130, 655, f"{load_num}")
+ticket.line(120, 652, 260, 652)
+ticket.drawString(30, 640, "Load of:")
+ticket.drawString(130, 640, f"{seed} {variety}")
+ticket.line(120, 637, 260, 637)
+ticket.drawString(30, 625, "From:")
+ticket.drawString(130, 625, f"{supplier}")
+ticket.line(120, 622, 260, 622)
+ticket.drawString(330, 640, "Wagon/Truck ID:")
+ticket.drawString(430, 640, f"{wagon}")
+ticket.line(420, 637, 560, 637)
+ticket.drawString(330, 625, "To:")
+ticket.drawString(430, 625, f"{destination}")
+ticket.line(420, 622, 560, 622)
+
+# write weights
+ticket.drawString(30, 595, "Weights:")
+ticket.drawString(30, 580, "Gross Weight:")
+ticket.drawString(130, 580, f"{gross_weight} lbs")
+ticket.line(120, 577, 260, 577)
+ticket.drawString(30, 565, "Tare Weight:")
+ticket.drawString(130, 565, f"{tare_weight} lbs")
+ticket.line(120, 562, 260, 562)
+ticket.drawString(30, 550, "Net Weight:")
+ticket.drawString(130, 550, f"{net_weight} lbs")
+ticket.line(120, 547, 260, 547)
+
+# write test values
+ticket.drawString(330, 595, "Test Weight:")
+ticket.drawString(430, 595, f"{test_weight}")
+ticket.line(420, 592, 560, 592)
+ticket.drawString(330, 580, "% Moisture:")
+ticket.drawString(430, 580, f"{moisture}")
+ticket.line(420, 577, 560, 577)
+ticket.drawString(330, 565, "% FM:")
+ticket.drawString(430, 565, f"{foreign_matter}")
+ticket.line(420, 562, 560, 562)
+ticket.drawString(330, 550, "% Cleanout:")
+ticket.drawString(430, 550, f"{cleanout}")
+ticket.line(420, 547, 560, 547)
+
+# write final values
+ticket.drawString(330, 505, "Gross Units:")
+ticket.drawString(430, 505, f"{gross_bushels} BU")
+ticket.line(420, 502, 560, 502)
+ticket.drawString(330, 490, "Net Units:")
+ticket.drawString(430, 490, f"{net_bushels} BU")
+ticket.line(420, 487, 560, 487)
+ticket.drawString(330, 460, "Verified By:")
+ticket.drawString(430, 460, f"{verified}")
+ticket.line(420, 457, 560, 457)
+
+ticket.save()
 
 # append scale ticket and supplier information to tickts.csv
 with open("tickets.csv", "a") as t:
-    t.write("\n"+str(Scale_Ticket) + ", " + str(Supplier))
+    t.write(f"\n{scale_ticket}, {supplier}")
 t.close()
